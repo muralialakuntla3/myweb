b2fd773726894dd3f6d17859ce306aca2a17a58e
Murali Krishna Alakuntla <118282852+muralialakuntla3@users.noreply.github.com>
2023-10-18 00:28:07 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 cd ~
 sudo apt-get update
 sudo apt install nginx -y
-sudo rm -f /var/www/html/*
+sudo rm -rf /var/www/html/*
 sudo cp -rf /home/ubuntu/myweb/* /var/www/html/
 sudo systemctl restart nginx
